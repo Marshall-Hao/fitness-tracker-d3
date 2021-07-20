eb1e5f924a0f5aab1c7ec3a780297cbc0fd05939
marshall chen <marshallchan666@outlook.com>
2021-07-20 12:06:20 +0800

    finish the little circle

--- javascript/graph.js
+++ javascript/graph.js
@@ -13,7 +13,65 @@ const graph = svg
   .attr("width", graphWidth)
   .attr("height", graphHeight)
   .attr("transform", `translate(${margin.left},${margin.top})`);
-const update = (data) => {};
+
+//todo: set scales
+const x = d3.scaleTime().range([0, graphHeight]);
+const y = d3.scaleLinear().range([graphHeight, 0]);
+
+// axes group
+const xAxisGroup = graph
+  .append("g")
+  .attr("class", "x-axis stroke-current text-white")
+  .attr("transform", `translate(0,${graphHeight})`);
+
+const yAxisGroup = graph
+  .append("g")
+  .attr("class", "y-axis stroke-current text-white");
+
+const update = (data) => {
+  //todo: set scale domain
+  x.domain(d3.extent(data, (d) => new Date(d.date)));
+  y.domain([0, d3.max(data, (d) => d.distance)]);
+
+  //todo:create circles for objects
+  const circles = graph.selectAll("circle").data(data);
+
+  //todo:update current points
+  circles
+    .attr("cx", (d) => x(new Date(d.date)))
+    .attr("cy", (d) => y(d.distance));
+  //todo:add new point with enter
+  circles
+    .enter()
+    .append("circle")
+    .attr("r", 4)
+    .attr("cx", (d) => x(new Date(d.date)))
+    .attr("cy", (d) => y(d.distance))
+    .attr("fill", "#ccc");
+
+  //todo:remove exit points
+  circles.exit().remove();
+
+  //todo:create axis
+  const xAxis = d3
+    .axisBottom(x)
+    .ticks(4)
+    .tickFormat(d3.timeFormat("%m-%e %H:%M"));
+  const yAxis = d3
+    .axisLeft(y)
+    .ticks(4)
+    .tickFormat((d) => d + "m");
+
+  //todo:call axes
+  xAxisGroup.call(xAxis);
+  yAxisGroup.call(yAxis); //*put the axis shape in the group by call
+
+  //todo:rotate axis text
+  xAxisGroup
+    .selectAll("text")
+    .attr("transform", "rotate(-40)")
+    .attr("text-anchor", "end");
+};
 var data = [];
 
 db.collection("activity").onSnapshot((res) => {
